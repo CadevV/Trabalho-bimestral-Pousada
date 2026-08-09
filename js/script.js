5dded77a0b966147c871bbb9534617d919cd7e8c
CadevV <vcaos110411@gmail.com>
2026-08-09 18:39:42 -0300

    Atualiza estilo e comportamento do site: adiciona seção de destaque com carrossel de acomodações e implementa navegação entre os itens

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const btnMenu = document.querySelector('.btnMenu')
 const btnMenuClose = document.querySelector('.btnMenuClose')
 const contMenu = document.querySelector('.contMenu')
+const carroselDesta = document.querySelector('.carroselDestaque')
+const btnLeftCard = document.querySelector('.btnPrevDestaque')
+const btnRightCard = document.querySelector('.btnNextDestaque')
+
+let indiceCarrosel = 0
 
 btnMenu.addEventListener('click', ()=>{
     contMenu.classList.add('active')
@@ -8,4 +13,30 @@ btnMenu.addEventListener('click', ()=>{
 
 btnMenuClose.addEventListener('click', ()=>{
     contMenu.classList.remove('active')
+})
+
+btnRightCard.addEventListener('click', () => {
+
+    if(indiceCarrosel < 2){
+        indiceCarrosel++
+    } else {
+        indiceCarrosel = 0
+    }
+
+    carroselDesta.style.transform =
+            `translateX(${-indiceCarrosel * 370}px)`
+
+})
+
+btnLeftCard.addEventListener('click', () => {
+
+    if(indiceCarrosel > 0){
+        indiceCarrosel--
+    } else {
+        indiceCarrosel = 2
+    }
+
+    carroselDesta.style.transform =
+            `translateX(${-indiceCarrosel * 370}px)`
+
 })
