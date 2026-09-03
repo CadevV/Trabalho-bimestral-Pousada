5c14b589f531220c9bb363e015c6a1a225805fd0
CadevV <vcaos110411@gmail.com>
2026-09-03 17:28:48 -0300

    atualizaçao geral das atraçoes melhorando desing geral e melhor funcionamento versoes finais

--- js/atracao.js
+++ js/atracao.js
@@ -1,27 +1,26 @@
-const btnMenu = document.querySelector('.btnMenu')
-const btnMenuClose = document.querySelector('.btnMenuClose')
-const contMenu = document.querySelector('.contMenu')
-const btnRedes = document.querySelectorAll('.btnRedes')
-const bntprograma = document.querySelector('.bntprograma')
-const respostaBnt = document.querySelector('.respostaBnt')
-const textoPrograma = document.querySelector('.programaTexto')
+const btnMenu = document.querySelector(".btnMenu");
+const btnMenuClose = document.querySelector(".btnMenuClose");
+const contMenu = document.querySelector(".contMenu");
+const btnRedes = document.querySelectorAll(".btnRedes");
+const bntprograma = document.querySelectorAll(".bntprograma");
+const textoPrograma = document.querySelectorAll(".programaTexto");
 
-btnMenu.addEventListener('click', ()=>{
-    contMenu.classList.add('active')
-})
+btnMenu.addEventListener("click", () => {
+  contMenu.classList.add("active");
+});
 
-btnMenuClose.addEventListener('click', ()=>{
-    contMenu.classList.remove('active')
-})
+btnMenuClose.addEventListener("click", () => {
+  contMenu.classList.remove("active");
+});
 
-btnRedes.forEach(element => {
-    element.addEventListener('click', () => {
-        window.location.href = element.dataset.url
-    })
-})
+btnRedes.forEach((element) => {
+  element.addEventListener("click", () => {
+    window.location.href = element.dataset.url;
+  });
+});
 
-bntprograma.addEventListener('click', () =>{
-    textoPrograma.classList.add('active')
-})
-
-
+bntprograma.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    textoPrograma[index].classList.toggle("active");
+  });
+});
